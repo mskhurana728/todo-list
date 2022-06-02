52105beb880906d3920f9a0b1ff06d4f45ddb267
Manpreet Singh Khurana <mskhurana728@gmail.com>
2022-06-01 22:22:49 -0400

    ADDED REMOVE BTN AND SOME CSS

--- src/modules/task.js
+++ src/modules/task.js
@@ -22,28 +22,49 @@ function displayProjectTasks(id) {
     const taskArea = document.querySelector(".taskArea");
     let i = 1;
     taskArea.textContent = "";
-    if (projects[id].length > 1) {
+  
         projects[id].forEach(project => {
-            if(project.tittle){
+            if (project.tittle) {
                 const task = document.createElement("div");
                 task.classList.add("task");
-                task.id=i;
+                task.id = i;
 
-                const taskInfo= document.createElement("p");
-                taskInfo.textContent=project.tittle +"  "+project.description+"  "+project.dueDate;
+                const taskInfo = document.createElement("p");
+                taskInfo.textContent = project.tittle + "  " + project.description + "  " + project.dueDate;
+                const removeTaskBtn = document.createElement("button");
+                removeTaskBtn.classList.add("removeTaskBtn");
+                removeTaskBtn.textContent = "X";
+                removeTaskBtn.id = i;
+                console.log(removeTaskBtn);
+
+
 
                 // const description = document.createElement("p");
                 task.appendChild(taskInfo);
+                task.appendChild(removeTaskBtn);
                 taskArea.appendChild(task);
 
 
-            // taskArea.textContent += project.tittle+"  ";
-            i++;
-        }
+                // taskArea.textContent += project.tittle+"  ";
+                i++;
+                // const removeTaskBtn = document.querySelector(".removeTaskBtn");
+                removeTaskBtn.addEventListener("click", () => {
+                    const index = removeTaskBtn.getAttribute("id");
+                    console.log(index);
+                    projects[id].splice(index, 1);
+                    displayProjectTasks(id);
+        
+                })
+            }
+          
+
+
         });
+       
     }
 
 
-}
+
+
 
 export { displayProjectTasks, task, addNewTask };
